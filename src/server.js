3c90a3dc31fbe039e9d6f11c23ae8af18b07cd60
rken2010 <rkenshin.2010@gmail.com>
2022-07-06 19:45:37 -0300

    se empieza a armar ruta de validacion

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const MongoStore = require('connect-mongo');
 const express = require ('express');
 const { engine: exphbs } = require ('express-handlebars');
 const session = require ('express-session');
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(
     session({
+        store: MongoStore.create( { mongoUrl: "mongodb://localhost/sessions" }),
+
         secret: 'abracadabra',
         resave: false,
         saveUninitialized: false,
@@ -24,23 +27,18 @@ app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main.hbs' }));
 app.set('view engine', '.hbs');
 
 /* CONFIGURAR PASSPORT */
-passport.use(`login`,new LocalStrategy( (username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-        if (err) {
-            return done (err);
-        if(!user) {
-            return done(null, false);
-        }
-        if(!isValidPassword(user, password)) {
-            return done(null, false);
-        }
-        return done(null, user);
-        }
-    })
-}))
+
 
 /* CONFIGURAR SERVER */
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+/* LISTEN SERVER */
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+server.on("error", error => console.log(`Error en servidor: ${error}`))
